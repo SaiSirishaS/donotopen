976b81fc43fea205b26a878d6f6b0670ff530200
Sai Sirisha <ssaisirisharaj@gmail.com>
2018-05-30 17:07:55 +0530

    First ejs project

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+// set the viwe engine
+app.set('view engine','ejs');
+
+// Root Route
 app.get('/', (req, res) => {
-    res.send("Hell O");
+    res.render('home')
 });
 
+
+// Always keep in end of file
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
